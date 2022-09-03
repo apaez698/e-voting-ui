29cf501f5349962e0b6aae95c9e342e70d62a43b
Sebastián Páez <sebastian.paez@rappi.com>
2022-09-03 10:45:56 -0500

    Add Automatic Deploy

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
 import useGetProducts from '../hooks/useGetProducts';
-const API = 'http://api.escuelajs.co/api/v1/products';
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
 	const products = useGetProducts(API);
